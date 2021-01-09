80e8c1c0dd2b3005399f456b34a3992b90703ca1
garciart <rgarcia@rgprogramming.com>
2021-01-08 22:38:53 -0500

    Corrected and tested Lab 1 code.

--- labs/g001_ping.py
+++ labs/g001_ping.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""Script 1: Ping the device from the host.
+"""Lab 1: Ping the device from the host.
 Make sure GNS3 is running first (gns3_run.sh)
 
-Using Python 2.7.5
+Project: Automation
+
+Requirements:
+- Python 2.7.5
 """
 from __future__ import print_function
 
+# Module metadata dunders
+__author__ = "Rob Garcia"
+__copyright__ = "Copyright 2020-2021, Rob Garcia"
+__email__ = "rgarcia@rgprogramming.com"
+__license__ = "MIT"
+
 import subprocess
+import sys
+
+import lab_utils as lu
+
+DEVICE_ADDRESS = "192.168.1.10"
 
 
 def main():
-    """Function to ping the device from the host."""
+    """Function to ping the device from the host.
+
+    :return: 0 if the function succeeded, 1 if it failed, or 2 if there was an error.
+    :rtype: int
+    """
+    rval = lu.FAIL
     try:
-        print("Script 1: Ping test...")
-        address = "192.168.1.10"
-        result = subprocess.call(["ping", "-c", "4", address])
+        result = subprocess.call(["ping", "-c", "4", DEVICE_ADDRESS])
         if result == 0:
-            print("Ping successful: Received a reply from {0}.".format(address))
+            print("Ping successful: Received a reply from {0}.".format(DEVICE_ADDRESS))
+            rval = lu.SUCCESS
         elif result == 1:
-            print("Ping unsuccessful: No reply from {0}.".format(address))
+            print("Ping unsuccessful: No reply from {0}.".format(DEVICE_ADDRESS))
         else:
             print("Ping unsuccessful: Error code {0}.".format(result))
-        print("Script complete. Have a nice day.")
-    except RuntimeError as ex:
-        print("Oops! Something went wrong:", ex)
+            rval = lu.ERROR
+        print("Lab complete. Have a nice day.")
+    except RuntimeError:
+        lu.log_error(sys.exc_info())
+        rval = lu.ERROR
+    return rval
 
 
 if __name__ == "__main__":
+    print("Lab 1: Pinging the device at {0} from the host...".format(DEVICE_ADDRESS))
     main()
