e038f4552dfb13febec01cdb0219d9bc21f0020b
garciart <rgarcia@rgprogramming.com>
2021-02-25 19:12:39 -0500

    Preparing demo script.

--- labs/g00_demo.py
+++ labs/g00_demo.py
@@ -33,72 +33,28 @@ logging.basicConfig(level=logging.NOTSET,
                     filename="labs.log",
                     format="%(asctime)sUTC: %(levelname)s:%(name)s:%(message)s")
 
-
-class Ramon3745(object):
-    @property
-    def host_ip_address(self):
-        return self._host_ip_address
-
-    @host_ip_address.setter
-    def host_ip_address(self, host_ip_address):
-        pass
-
-    @property
-    def device_ip_address(self):
-        return self._device_ip_address
-
-    @property
-    def subnet_mask(self):
-        return self._subnet_mask
-
-    @property
-    def start_up_config_file(self):
-        return self._start_up_config_file
-
-    def __init__(self, host_ip_address, device_ip_address, subnet_mask, start_up_config_file):
-        self._host_ip_address = host_ip_address
-        self._device_ip_address = device_ip_address
-        self._subnet_mask = subnet_mask
-        self._start_up_config_file = start_up_config_file
-
-    def run(self):
-        try:
-            print("Hello from Cisco Ramon!")
-            #if self._is_gns3_running():
-                #print("GNS3 is running: Check")
-            if self._is_the_lab_loaded():
-                print("Lab 0 is loaded and started: Check")
-        except BaseException:
-            print(sys.exc_info())
-
-    @staticmethod
-    def _is_gns3_running():
-        child_result, child_exitstatus = pexpect.run("pgrep gns3server", timeout=30, withexitstatus=True)
-        if child_exitstatus == 0:
-            return True
-        else:
-            raise RuntimeError("GNS3 is not running. " +
-                               "Please run ./gns3_run.sh to start GNS3 before executing this script.")
-
-    def _is_the_lab_loaded(self):
-        child_result, child_exitstatus = pexpect.run("ping -c 4 {0}".format(self._device_ip_address),
-                                                     timeout=30, withexitstatus=True)
-        if child_exitstatus == 0:
-            return True
-        else:
-            raise RuntimeError("Unable to reach device." +
-                               "Please load Lab 0 in GNS3 and start all devices before executing this script.")
-
-    def _connect_to_device(self):
-        child = pexpect.spawn("telnet {0}".format(self._device_ip_address))
-
-    def _enable_tftp(self):
-        pass
-
-    def _disbale_tftp(self):
-        pass
-
-
-if __name__ == "__main__":
-    r3745 = Ramon3745("192.168.1.100", "192.168.1.10", "255.255.255.0", "")
-    r3745.run()
+child = pexpect.spawn("telnet 192.168.1.100 5001")
+time.sleep(30)
+child.sendline()
+child.sendline("enable ! Enter Priviledged EXEC Mode")
+child.expect_exact("R1#")
+child.sendline("configure terminal ! Enter Global Configuration Mode")
+child.expect_exact("R1(config)#")
+child.sendline("interface FastEthernet0/0  ! Enter Interface Configuration Mode")
+child.expect_exact("R1(config-if)#")
+child.sendline("ip address 192.168.1.10 255.255.255.0 ! Set the IP address of the router")
+child.expect_exact("R1(config-if)#")
+child.sendline("no shutdown ! Bring up the interface")
+child.expect_exact("R1(config-if)#")
+child.sendline("exit ! Exit Interface Configuration Mode")
+child.expect_exact("R1(config)#")
+child.sendline("ip route 0.0.0.0 0.0.0.0 192.168.1.100 ! Configure the default gateway")
+child.expect_exact("R1(config)#")
+child.sendline("end ! Exit Global Configuration Mode")
+child.expect_exact("R1#")
+child.sendline("write memory ! Save new configuration to flash memory")
+child.expect_exact("R1#")
+child.sendline("copy running-config startup-config ! Use the current configuration for startup")
+child.expect_exact("R1#")
+child.sendline("exit")
+print("Script complete. Have a nice day.")
