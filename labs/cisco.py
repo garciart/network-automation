99550c88bd3549a34603f4a1871bf420daf770db
gns3user <gns3user@localhost.localdomain>
2022-02-28 20:21:24 -0500

    Tested cisco.py on a configured and unconfigured device.

--- labs/cisco.py
+++ labs/cisco.py
@@ -15,7 +15,6 @@ import re
 import sys
 import time
 from datetime import datetime
-from getpass import getpass
 
 # Module metadata dunders
 __author__ = "Rob Garcia"
@@ -44,7 +43,8 @@ YLW = "\x1b[33m"
 
 class Cisco(object):
     _cisco_prompts = [
-        ">", "#", "(config)#", "(config-if)#", "(config-router)#", "(config-line)#", ]
+        ">", "#", "(config)#", "(config-if)#", "(config-line)#", "(config-switch)#",
+        "(config-router)#", ]
 
     _device_hostname = None
     _device_prompts = None
@@ -56,7 +56,7 @@ class Cisco(object):
     _aux_password = None
     _enable_password = None
 
-    _eol = ""
+    _eol = "\r"
 
     def __init__(self, device_hostname,
                  device_ip_addr=None,
@@ -100,7 +100,8 @@ class Cisco(object):
         self._enable_password = enable_password
         self._eol = eol
 
-    def connect_via_telnet(self, device_ip_addr=_device_ip_addr, port_number=_port_number):
+    def connect_via_telnet(self, device_ip_addr=_device_ip_addr, port_number=_port_number,
+                           verbose=True):
         utility.validate_ip_address(device_ip_addr)
         utility.validate_port_number(port_number)
         print("Checking Telnet client is installed...")
@@ -110,8 +111,19 @@ class Cisco(object):
         print("Telnet client is installed.")
         print("Connecting to {0} on port {1} via Telnet...".format(device_ip_addr, port_number))
         child = pexpect.spawn("telnet {0} {1}".format(device_ip_addr, port_number))
-        # Accept changes (e.g., delay, logfile, etc.) to child object
-        child = self.clear_startup_msgs(child)
+        # Slow down commands to prevent race conditions with output
+        child.delaybeforesend = 0.5
+        if verbose:
+            # Echo both input and output to the screen
+            child.logfile = sys.stdout
+        else:
+            # Save output to file
+            fout = open("{0}-{1}".format(
+                self._device_hostname, datetime.utcnow().strftime("%y%m%d-%H%M%SZ")), "wb")
+            child.logfile = fout
+        # Get to Privileged EXEC Mode
+        self._clear_startup_msgs(child)
+        self.access_priv_exec_mode(child)
         print("Connected to {0} on port {1} via Telnet.".format(device_ip_addr, port_number))
         return child
 
@@ -121,8 +133,8 @@ class Cisco(object):
                            data_bits=8,
                            parity="n",
                            stop_bit=1,
-                           flow_control="N"):
-        child = None
+                           flow_control="N",
+                           verbose=True):
         print("Checking if a terminal emulator is installed...")
         _, exitstatus = pexpect.run("which putty", withexitstatus=True)
         if exitstatus != 0:
@@ -142,16 +154,23 @@ class Cisco(object):
                 serial_device, baud_rate, data_bits, parity, stop_bit, flow_control)
         print(cmd)
         child = pexpect.spawn(cmd)
-        # Accept changes (e.g., delay, logfile, etc.) to child object
-        child = self.clear_startup_msgs(child)
-        return child
-
-    def clear_startup_msgs(self, child, username=_vty_username, password=_vty_password):
         # Slow down commands to prevent race conditions with output
         child.delaybeforesend = 0.5
-        # Echo both input and output to the screen
-        child.logfile_read = sys.stdout
+        if verbose:
+            # Echo both input and output to the screen
+            child.logfile = sys.stdout
+        else:
+            # Save output to file
+            fout = open("{0}-{1}".format(
+                self._device_hostname, datetime.utcnow().strftime("%y%m%d-%H%M%SZ")), "wb")
+            child.logfile = fout
+        # Get to Privileged EXEC Mode
+        self._clear_startup_msgs(child)
+        self.access_priv_exec_mode(child)
+        return child
 
+    def _clear_startup_msgs(self, child, username=_vty_username, password=_vty_password, eol=_eol):
+        # MUST BE RUN IMMEDIATELY AFTER PEXPECT.SPAWN!
         # noinspection PyTypeChecker
         index = child.expect_exact([pexpect.TIMEOUT, ] + self._device_prompts, 1)
         if index != 0:
@@ -160,12 +179,8 @@ class Cisco(object):
             print("\x1b[31;1mYou may be accessing an open or uncleared virtual teletype " +
                   "session.\nOutput from previous commands may cause pexpect searches to fail.\n" +
                   "To prevent this in the future, reload the device to clear any artifacts.\x1b[0m")
-            # Move the pexpect cursor forward to the newest hostname prompt
-            tracer_round = ";{0}".format(int(time.time()))
-            # Add the carriage return here, not in the tracer_round.
-            # Otherwise, you won't find the tracer_round later
-            child.sendline(tracer_round + self._eol)
-            child.expect_exact("{0}".format(tracer_round), timeout=1)
+            self._set_pexpect_cursor(child)
+            return
         # Always try to find hostname prompts before anything else
         index_offset = len(self._device_prompts)
         while True:
@@ -194,14 +209,281 @@ class Cisco(object):
                     # child.sendline(
                     #       (password if password is not None else getpass(
                     #       "Enter password: ")) + eol)
-                child.sendline(password + self._eol)
+                child.sendline(password + eol)
             elif index == index_offset + 4:
-                child.sendline("no" + self._eol)
+                child.sendline("no" + eol)
             elif index == index_offset + 5:
-                child.sendline("yes" + self._eol)
+                child.sendline("yes" + eol)
             elif index == index_offset + 6:
-                child.sendline(self._eol)
-        return child
+                child.sendline(eol)
+
+    def access_priv_exec_mode(self, child, enable_password=_enable_password, eol=_eol):
+        self._set_pexpect_cursor(child)
+
+        print("Accessing Privileged EXEC Mode...")
+        child.sendline(eol)
+        index = child.expect_exact(self._device_prompts, 1)
+        if index == 0:
+            child.sendline("enable" + eol)
+            index = child.expect_exact(["Password:", self._device_prompts[1], ], 1)
+            if index == 0:
+                child.sendline(enable_password + eol)
+                child.expect_exact(self._device_prompts[1], 1)
+        elif index != 1:
+            child.sendline("end" + eol)
+            child.expect_exact(self._device_prompts[1], 1)
+        print("Privileged EXEC Mode accessed.")
+
+    def format_memory(self, child, disk_name="flash", eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Formatting device memory...")
+        # Format the memory. Look for the final characters of the following strings:
+        # "Format operation may take a while. Continue? [confirm]"
+        # "Format operation will destroy all data in "flash:".  Continue? [confirm]"
+        # "66875392 bytes available (0 bytes used)"
+        child.sendline("format {0}:".format(disk_name) + eol)
+        child.expect_exact("Continue? [confirm]")
+        child.sendline(eol)
+        child.expect_exact("Continue? [confirm]")
+        child.sendline(eol)
+        # Not all devices ask for this
+        index = child.expect_exact(["Enter volume ID", pexpect.TIMEOUT, ], timeout=1)
+        if index == 0:
+            child.sendline(eol)
+        child.expect_exact("Format of {0} complete".format(disk_name), timeout=120)
+        child.sendline("show {0}".format(disk_name) + eol)
+        child.expect_exact("(0 bytes used)")
+        child.expect_exact(self._device_prompts[1])
+        print("Device memory formatted.")
+
+    def get_device_info(self, child, eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Getting device information...")
+        child.sendline("show version | include [IOSios] [Ss]oftware" + eol)
+        child.expect_exact(self._device_prompts[1])
+
+        software_ver = str(child.before).split(
+            "show version | include [IOSios] [Ss]oftware\r")[1].split("\r")[0].strip()
+        if not re.compile(r"[IOSios] [Ss]oftware").search(software_ver):
+            raise RuntimeError("Cannot get the device's software version.")
+        print("Software version: {0}".format(software_ver))
+
+        child.sendline("show inventory | include [Cc]hassis" + eol)
+        child.expect_exact(self._device_prompts[1])
+        # child.expect_exact(device_prompts[1])
+        device_name = str(child.before).split(
+            "show inventory | include [Cc]hassis\r")[1].split("\r")[0].strip()
+        if not re.compile(r"[Cc]hassis").search(device_name):
+            raise RuntimeError("Cannot get the device's name.")
+        print("Device name: {0}".format(device_name))
+
+        child.sendline("show version | include [Pp]rocessor [Bb]oard [IDid]" + eol)
+        child.expect_exact(self._device_prompts[1])
+        # child.expect_exact(device_prompts[1])
+        serial_num = str(child.before).split(
+            "show version | include [Pp]rocessor [Bb]oard [IDid]\r")[1].split("\r")[0].strip()
+        if not re.compile(r"[Pp]rocessor [Bb]oard [IDid]").search(serial_num):
+            raise RuntimeError("Cannot get the device's serial number.")
+        print("Serial number: {0}".format(serial_num))
+
+    def set_device_ip_addr(self, child, new_ip_address, new_netmask="255.255.255.0", commit=True,
+                           eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Setting the device's IP address...")
+        child.sendline("configure terminal" + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("interface FastEthernet0/0" + eol)
+        child.expect_exact(self._device_prompts[3])
+        child.sendline("ip address {0} {1}".format(new_ip_address, new_netmask) + eol)
+        child.expect_exact(self._device_prompts[3])
+        child.sendline("no shutdown" + eol)
+        child.expect_exact(self._device_prompts[3])
+        child.sendline("end" + eol)
+        child.expect_exact(self._device_prompts[1])
+        # Save changes if True
+        if commit:
+            child.sendline("write memory" + eol)
+            child.expect_exact(self._device_prompts[1])
+        print("Device IP address set.")
+
+    def ping_from_device(self, child, destination_ip_addr, count=4, eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Pinging {0}...".format(destination_ip_addr))
+        child.sendline("ping {0} repeat {1}".format(destination_ip_addr, count) + eol)
+        index = child.expect(["percent (0/4)", r"percent \([1-4]/4\)", ])
+        if index == 0:
+            raise RuntimeError("Cannot ping {0} from this device.".format(destination_ip_addr))
+        print("Pinged {0}.".format(destination_ip_addr))
+
+    @staticmethod
+    def ping_device(device_ip_addr, count=4):
+        print("Pinging {0}...".format(device_ip_addr))
+        _, exitstatus = pexpect.run("ping -c {0} {1}".format(count, device_ip_addr),
+                                    withexitstatus=True)
+        if exitstatus != 0:
+            # No need to read the output. Ping returns a non-zero value if no packets are received
+            raise RuntimeError("Cannot ping the device at {0}.".format(device_ip_addr))
+        print("Pinged {0}.".format(device_ip_addr))
+
+    def secure_device(self, child, vty_username=_vty_username, vty_password=_vty_password,
+                      privilege=15, console_password=_console_password, aux_password=_aux_password,
+                      enable_password=_enable_password, commit=True, eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Securing the network device...")
+
+        child.sendline("configure terminal" + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Secure the device with a username and an unencrypted password
+        child.sendline("username {0} password {1}".format(vty_username, vty_password) + eol)
+        # If the device already is configured with a user secret, you may see:
+        # ERROR: Can not have both a user password and a user secret.
+        # Please choose one or the other.
+        # That is OK for this lab
+        child.expect_exact(self._device_prompts[2])
+
+        # Set virtual teletype lines to use device username and password
+        child.sendline("line vty 0 4" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("login local" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("exit" + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Secure console port connections
+        child.sendline("line console 0" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("password {0}".format(console_password) + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("login" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("exit" + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Secure auxiliary port connections
+        child.sendline("line aux 0" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("password {0}".format(aux_password) + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("login" + eol)
+        child.expect_exact(self._device_prompts[4])
+        child.sendline("exit" + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Secure Privileged EXEC Mode
+        child.sendline("enable password {0}".format(enable_password) + eol)
+        # If the device already is configured with an enable secret, you may see:
+        # The enable password you have chosen is the same as your enable secret.
+        # This is not recommended.  Re-enter the enable password.
+        # That is OK for this lab
+        child.expect_exact(self._device_prompts[2])
+        # Test security
+        child.sendline("end" + eol)
+        child.expect_exact(self._device_prompts[1])
+        child.sendline("disable" + eol)
+        child.expect_exact(self._device_prompts[0])
+        child.sendline("enable" + eol)
+        child.expect_exact("Password:")
+        child.sendline("{0}".format(enable_password) + eol)
+        child.expect_exact(self._device_prompts[1])
+        child.sendline("configure terminal" + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Encrypt the Privileged EXEC Mode password
+        child.sendline("no enable password" + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("enable secret {0}".format(enable_password) + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Encrypt the device's password
+        child.sendline(
+            "no username {0} password {1}".format(vty_username, vty_password) + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("username {0} privilege {1} secret {2}".format(vty_username, privilege,
+                                                                      vty_password) + eol)
+        child.expect_exact(self._device_prompts[2])
+
+        # Encrypt the console and auxiliary port passwords
+        child.sendline("service password-encryption" + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("end" + eol)
+        child.expect_exact(self._device_prompts[1])
+
+        # Save changes if True
+        if commit:
+            child.sendline("write memory" + eol)
+            child.expect_exact(self._device_prompts[1])
+        print("Network device secured." + eol)
+
+    def set_clock(self, child, time_to_set="12:00:00 Jan 1 2021", eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Setting the network device's clock." + eol)
+        child.sendline("clock set {0}".format(time_to_set) + eol)
+        child.expect_exact(self._device_prompts[1])
+        print("Network device clock set." + eol)
+
+    def synch_clock(self, child, ntp_server_ip, commit=True, eol=_eol):
+        self.access_priv_exec_mode(child)
+
+        print("Synchronizing the network device's clock." + eol)
+        child.sendline("configure terminal" + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("ntp server {0}".format(ntp_server_ip) + eol)
+        child.expect_exact(self._device_prompts[2])
+        child.sendline("end" + eol)
+        child.expect_exact(self._device_prompts[1])
+        # Save changes if True
+        if commit:
+            child.sendline("write memory" + eol)
+            child.expect_exact(self._device_prompts[1])
+        print("Waiting 60 seconds for the NTP server to synchronize...")
+        time.sleep(60)
+        print("Network device clock synchronized." + eol)
+
+    def _set_pexpect_cursor(self, child, eol=_eol):
+        # Move the pexpect cursor forward to the newest hostname prompt
+        tracer_round = ";{0}".format(int(time.time()))
+        # Add a carriage return here, not in the tracer_round,
+        # or you won't find the tracer_round later
+        child.sendline(tracer_round + eol)
+        child.expect_exact("{0}".format(tracer_round), timeout=1)
+        # WATCH YOUR CURSORS! You must also consume the prompt after the tracer round
+        # or the pexepect cursor will stop at the wrong prompt
+        # The next cursor will stop here -> R2#show version | include [IOSios] [Ss]oftware
+        #                                   Cisco IOS Software...
+        #      But it needs to stop here -> R2#
+        child.expect_exact(self._device_prompts, 1)
+
+    @staticmethod
+    def close_telnet(child, eol=_eol):
+        print("Closing Telnet connection...")
+        child.sendcontrol("]")
+        child.expect_exact("telnet>")
+        child.sendline("q" + eol)
+        child.expect_exact(["Connection closed.", pexpect.EOF, ])
+        child.close()
+        print("Telnet connection closed")
+
+    @staticmethod
+    def close_putty(child):
+        print("Closing PuTTY session...")
+        child.close()
+        print("PuTTY session closed")
+
+    @staticmethod
+    def close_ssh(child, device_ip_addr=_device_ip_addr, eol=_eol):
+        utility.validate_ip_address(device_ip_addr)
+        print("Closing SSH session...")
+        child.sendline("~." + eol)
+        child.expect_exact(["Connection to {0} closed.".format(device_ip_addr), pexpect.EOF, ])
+        child.close()
+        print("SSH session closed")
 
 
 if __name__ == "__main__":
